7f93dd6ac44c20ba95901ae3793603f7ee64f0c9
Xinyuan Chen <xinyuan.chen.24@ucl.ac.uk>
2025-06-08 19:51:52 +0100

    create modelConfig for training and eval

--- main.py
+++ main.py
@@ -89,4 +89,33 @@ def eval(modelConfig: Dict):
 
 
 if __name__ == '__main__':
-    pass
+    modelConfig = {
+        "state": "train",  # or eval
+        "epoch": 200,
+        "batch_size": 80,
+        "T": 1000,
+        "channel": 128,
+        "channel_mult": [1, 2, 3, 4],
+        "attn": [2],
+        "num_res_blocks": 2,
+        "dropout": 0.15,
+        "lr": 1e-4,
+        "multiplier": 2.,
+        "beta_1": 1e-4,
+        "beta_T": 0.02,
+        "img_size": 32,
+        "grad_clip": 1.,
+        "device": "cuda:0",  ### MAKE SURE YOU HAVE A GPU !!!
+        "training_load_weight": None,
+        "save_weight_dir": "./Checkpoints/",
+        "test_load_weight": "ckpt_199_.pt",
+        "sampled_dir": "./SampledImgs/",
+        "sampledNoisyImgName": "NoisyNoGuidenceImgs.png",
+        "sampledImgName": "SampledNoGuidenceImgs.png",
+        "nrow": 8
+    }
+
+    if modelConfig["state"] == "train":
+        train(modelConfig)
+    else:
+        eval(modelConfig)
