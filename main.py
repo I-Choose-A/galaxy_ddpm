5a505d3d3b7518e3c87e8da802f973c9dc4c78ee
Xinyuan Chen <xinyuan.chen.24@ucl.ac.uk>
2025-08-20 05:27:46 +0100

    Modify the training process and sampling process. Remove custom scheduler.

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import os
 
 import torch
-import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
 import numpy as np
-from diffusion import GaussianDiffusionSampler, GaussianDiffusionTrainer
-from unet import UNet
-from scheduler import GradualWarmupScheduler
+from models.diffusion import GaussianDiffusionSampler, GaussianDiffusionTrainer
+from models.unet import UNet
 from dataset import SDSS
 
 modelConfig = {
     "state": "train",  # or sampling
-    "epoch": 10,
-    "batch_size": 64,
+    "epoch": 100,
+    "batch_size": 128,
     "T": 1000,
     "num_img_channel": 5,
     "selected_channel": ["u", "g", "r", "i", "z"],
@@ -23,7 +21,7 @@ modelConfig = {
     "channel_mult": [1, 2, 3, 4],
     "num_res_blocks": 1,
     "dropout": 0.1,
-    "lr": 5e-5,
+    "lr": 5e-6,
     "multiplier": 2.,
     "beta_1": 1e-4,
     "beta_T": 0.02,
@@ -32,12 +30,11 @@ modelConfig = {
     "device": "cuda",
     "training_load_weight": "ckpt_29_no-transfer_5ch_lr5e-5_conditional.pt",
     "save_weight_dir": "./Checkpoints/",
-    "test_load_weight": "ckpt_29_.pt",
+    "test_load_weight": "ckpt_29_no-transfer_5ch_lr5e-5_conditional.pt",
     "sampled_dir": "./SampledImgs/",
     "sampledNoisyImgName": "NoisyNoGuidenceImgs.png",
     "sampledImgName": "SampledNoGuidenceImgs.png",
 
-    # myriad use
     "images_path": r"/home/ucaphey/Scratch/sdss.npz",
     "conditions_path": r"/home/ucaphey/Scratch/sdss_morphology_labels.csv",
 }
@@ -51,6 +48,7 @@ def train():
     sep_mean = [0.006874967832118273, 0.03785848245024681, 0.07799547165632248, 0.1106007769703865, 0.13126179575920105]
     sep_std = [0.08303029835224152, 0.255173921585083, 0.46921947598457336, 0.6261722445487976, 0.8178646564483643]
 
+    # remove bad data -> norm -> tanh
     astronomical_transform = transforms.Compose([
         transforms.Lambda(lambda x: np.nan_to_num(x, nan=0.0)),
         transforms.Lambda(lambda x: np.clip(x, -0.999, 1000)),
@@ -86,22 +84,13 @@ def train():
         num_features=modelConfig["num_features"]
     ).to(device)
 
+    # load pretrained model
     if modelConfig["training_load_weight"] is not None:
         pretrained_dict = torch.load(
             os.path.join(modelConfig["save_weight_dir"], modelConfig["training_load_weight"]),
             map_location=device
         )
         net_model.load_state_dict(pretrained_dict)
-        # model_dict = net_model.state_dict()
-
-        # #  filter the layers according to condition embedding
-        # pretrained_dict = {
-        #     k: v for k, v in pretrained_dict.items()
-        #     if k in model_dict and "condition_embedding" not in k
-        # }
-        # model_dict.update(pretrained_dict)
-        # net_model.load_state_dict(model_dict, strict=False)
-        # print(f"Loaded pretrained weights from {modelConfig['training_load_weight']}")
 
     # Optimizer setup
     optimizer = torch.optim.AdamW(
@@ -110,20 +99,7 @@ def train():
         weight_decay=1e-4
     )
 
-    # cosineScheduler = optim.lr_scheduler.CosineAnnealingLR(
-    #     optimizer=optimizer,
-    #     T_max=modelConfig["epoch"],
-    #     eta_min=0,
-    #     last_epoch=-1
-    # )
-    # warmUpScheduler = GradualWarmupScheduler(
-    #     optimizer=optimizer,
-    #     multiplier=modelConfig["multiplier"],
-    #     # multiplier=1,
-    #     warm_epoch=modelConfig["epoch"] // 10,
-    #     after_scheduler=cosineScheduler
-    # )
-
+    # init trainer class for training ddpm
     trainer = GaussianDiffusionTrainer(
         net_model,
         modelConfig["beta_1"],
@@ -131,7 +107,7 @@ def train():
         modelConfig["T"]
     ).to(device)
 
-    # Training loop
+    # Training process
     print(f"Training started for {modelConfig['epoch']} epochs")
     for epoch in range(modelConfig["epoch"]):
         epoch_loss = 0.0
@@ -201,8 +177,8 @@ def sampling():
             modelConfig["T"]
         ).to(device)
 
-        # real_images_per_class = [36478, 37912, 4005, 15480, 82730]
-        real_images_per_class = [10, 10, 10, 10, 10]
+        # 1/8 of real dataset
+        real_images_per_class = [9120, 4978, 1000, 3870, 20682]
 
         batch_size = modelConfig["batch_size"]
         output_dir = os.path.join(modelConfig["sampled_dir"], "fid_batches/using_feature_median")
@@ -224,16 +200,11 @@ def sampling():
                     [current_batch_size, modelConfig["num_img_channel"], 64, 64],
                     device=device
                 )
-                # use mean of each physical feature
-                # c = torch.tensor([4.43992601133607, 0.011444474540351243, -6.1040309726109205, 0.0803721621632576,
-                #                   10.753847908103175, class_id] * current_batch_size, device=device)
-                # use median of each physical feature
+
+                # median value of each physical feature
                 c = torch.tensor([3.8961502088838444, 0.008839854973913385, -2.7289007776677927, 0.07603080570697784,
                                   10.844648974261666, class_id] * current_batch_size, device=device)
-                # custom
-                # c = torch.tensor(
-                #     [13.061960593453904, 0.030131004366812226, -32.729402869739836, 0.3845420479774475, 12.479516789074125,
-                #      class_id] * current_batch_size, device=device)
+
 
                 c = c.view(current_batch_size, -1)
                 # sampling
@@ -242,7 +213,7 @@ def sampling():
                 # save batch to temp file
                 batch_path = os.path.join(
                     output_dir,
-                    f"class_{class_id}_batch_{batch_idx:04d}_10samples.npy"  # Zero-padded naming
+                    f"class_{class_id}_batch_{batch_idx:04d}_Median.npy"  # Zero-padded naming
                 )
                 np.save(batch_path, sampled.cpu().numpy())
 
@@ -250,28 +221,28 @@ def sampling():
                 if batch_idx % 5 == 0 and torch.cuda.is_available():
                     torch.cuda.empty_cache()
 
-        # print("\nOrganizing files for cFID evaluation...")
-        # final_output_dir = os.path.join(modelConfig["sampled_dir"], "cFID_results")
-        # os.makedirs(final_output_dir, exist_ok=True)
-        # 
-        # for class_id in range(modelConfig["num_classes"]):
-        #     # load all batches for current class
-        #     class_files = sorted([
-        #         os.path.join(output_dir, f)
-        #         for f in os.listdir(output_dir)
-        #         if f.startswith(f"class_{class_id}_") and f.endswith(".npy")
-        #     ])
-        # 
-        #     # concatenate and save as single .npy file
-        #     class_images = np.concatenate(
-        #         [np.load(f) for f in class_files],
-        #         axis=0
-        #     )
-        #     np.save(
-        #         os.path.join(final_output_dir, f"generated_class_{class_id}.npy"),
-        #         class_images
-        #     )
-        #     print(f"Class {class_id}: Save {len(class_images)} images")
+        print("\nOrganizing files for cFID evaluation...")
+        final_output_dir = os.path.join(modelConfig["sampled_dir"], "results")
+        os.makedirs(final_output_dir, exist_ok=True)
+
+        for class_id in range(modelConfig["num_classes"]):
+            # load all batches for current class
+            class_files = sorted([
+                os.path.join(output_dir, f)
+                for f in os.listdir(output_dir)
+                if f.startswith(f"class_{class_id}_") and f.endswith(".npy")
+            ])
+
+            # concatenate and save as single .npy file
+            class_images = np.concatenate(
+                [np.load(f) for f in class_files],
+                axis=0
+            )
+            np.save(
+                os.path.join(final_output_dir, f"generated_class_{class_id}_Median.npy"),
+                class_images
+            )
+            print(f"Class {class_id}: Save {len(class_images)} images")
 
 
 if __name__ == '__main__':
